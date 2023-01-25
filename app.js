3b2e9e5930e157e3f5e9b9e4e0c1d0706f21daac
aytheotaku <ayakinbo@gmail.com>
2023-01-25 20:43:00 +0100

    corrected view engine bug

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const app = express();
 const PORT_NUMBER = 3000;
 
 // telling express where to get static files to be served
+app.set("view engine", "ejs")
+
 app.use(express.static('public'))
 
 //telling express to use ejs as template engine
-app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.render("index");
+});
 
 
 app.listen(PORT_NUMBER, () => console.log(`Now listening at port ${PORT_NUMBER}`))
