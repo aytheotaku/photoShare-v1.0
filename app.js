07dc36b35f651406c2df5d36dba4527e2182faba
aytheotaku <ayakinbo@gmail.com>
2023-01-25 20:06:57 +0100

    set view engine to ejs

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const PORT_NUMBER = 3000;
 
+// telling express where to get static files to be served
 app.use(express.static('public'))
 
-app.get('/', (req, res) => console.log('Hello World'))
+//telling express to use ejs as template engine
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 
-app.listen(PORT_NUMBER, () => {`Now listening at port ${PORT_NUMBER}`})
+app.listen(PORT_NUMBER, () => console.log(`Now listening at port ${PORT_NUMBER}`))
