eeb0f643d779fb5f51c6bc46afab10488806397a
aytheotaku <ayakinbo@gmail.com>
2023-01-26 13:58:51 +0100

    added database models for users and config files

---
+++ models/UsersModel.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define(
+        "user",
+        {
+            id:{
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            email: {
+                type: DataTypes.STRING, 
+
+            },
+            password: {
+                type: DataTypes.STRING, 
+
+            },
+            username: {
+                type: DataTypes.STRING, 
+
+            },
+        }
+    )
+    return Users
+} 
