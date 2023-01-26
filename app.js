2e7d29a51222ed46bb285e842fd584c4f2b6d510
aytheotaku <ayakinbo@gmail.com>
2023-01-26 14:00:15 +0100

    required body-parser dependency and required models

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const app = express();
-const PORT_NUMBER = 3000;
+const db = require('./models');
+const bodyParser = require('body-parser');
 
-// telling express where to get static files to be served
+
+
+app.use(bodyParser.json())
+// teling express where to get static files to be served
 app.use(express.static('public'))
+//telling express to use ejs as template engine
 app.set("view engine", "ejs")
 
-
-//telling express to use ejs as template engine
-
+  
+//home route
 app.get('/', (req, res) => {
     res.render("index");
 });
 
 
+//connecting to database
+const sqlPort = 3305 
+db.sequelize
+    .sync({})
+    .then(() => {
+        app.listen(sqlPort, () => console.log(`Maria db connection successful at ${sqlPort}`))
+
+    })
+    .catch((err) => console.error("Unable to connect to the database"))
+
+
+
+
+//SERVER
+const PORT_NUMBER = 3000;
 app.listen(PORT_NUMBER, () => console.log(`Now listening at port ${PORT_NUMBER}`))
